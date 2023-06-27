2bcc7e437ae1b1bda30a2aab206892bb625a7239
Matteo Pasqualetti <matteo.pasqualetti@libero.it>
2023-06-27 19:20:12 +0200

    Java skeleton (no maven stuff)

---
+++ F1_Application_Controller/src/main/java/it/unipi/iot/App.java
@@ -0,0 +1,13 @@
+package it.unipi.iot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
