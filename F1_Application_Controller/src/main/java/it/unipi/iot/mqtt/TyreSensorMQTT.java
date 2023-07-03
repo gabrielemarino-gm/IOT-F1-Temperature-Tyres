32b4e52455f836c8d3554e0a3c325a23041c54d4
Gabriele Marino <45176656+gabrielemarino-gm@users.noreply.github.com>
2023-07-03 18:49:28 +0200

    Update TyreSensorMQTT.java

--- F1_Application_Controller/src/main/java/it/unipi/iot/mqtt/TyreSensorMQTT.java
+++ F1_Application_Controller/src/main/java/it/unipi/iot/mqtt/TyreSensorMQTT.java
@@ -75,7 +75,7 @@ public class TyreSensorMQTT
                 temp.setTyrePosition(Integer.parseInt(args[0].split("=")[1]));
                 temp.setTemperatureValue(Double.parseDouble(args[1].split("=")[1]) / 10);
                 Actuator act = TemperatureDAO.getActuator(temp.getTyrePosition(), topic);
-                // act.setStatus("UNDEFINED");
+                act.setStatus("UNDEFINED");
                 System.out.println(String.format("DBG   Temperature = %s", "" + temp.getTemperatureValue()));
 
                 try
