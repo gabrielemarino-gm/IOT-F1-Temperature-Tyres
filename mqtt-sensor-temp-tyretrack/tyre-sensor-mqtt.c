cd7cf25e1ecbf355c4e29ff93b28d254fedccad0
Gabriele Marino <45176656+gabrielemarino-gm@users.noreply.github.com>
2023-07-07 10:32:54 +0200

    Update tyre-sensor-mqtt.c

--- mqtt-sensor-temp-tyretrack/tyre-sensor-mqtt.c
+++ mqtt-sensor-temp-tyretrack/tyre-sensor-mqtt.c
@@ -157,7 +157,7 @@ static void handler_incoming_msg(const char *topic, const uint8_t *chunk)
     
     LOG_DBG("(int)*msg_ptr->payload_chunk = %d\n", atoi((const char *)msg_ptr->payload_chunk));
     
-    state_machine_timer = (CLOCK_SECOND * atoi((int)*msg_ptr->payload_chunk));
+    state_machine_timer = (CLOCK_SECOND * atoi((const char *)msg_ptr->payload_chunk));
     //etimer_set(&periodic_state_timer, state_machine_timer);
     //etimer_reset(&periodic_state_timer);
     etimer_adjust(&periodic_state_timer, state_machine_timer);
